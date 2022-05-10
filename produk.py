0c2c24473bd306f978e05eb425216bd0eef0c858
damiri127 <damiricepri127@gmail.com>
2022-05-10 15:13:53 +0700

    perubahan 1

--- produk.py
+++ produk.py
@@ -2,11 +2,18 @@ from abc import ABC, abstractmethod
 
 class Produk:
     
-    def __init__(self, nama, harga):
+    def set_nama(self, nama : str):
         self.__nama = nama
+        
+    def get_nama(self):
+        return self.__nama
+    
+    def set_harga(self, harga :float):
         self.__harga = harga
         
-    
+    def get_harga(self):
+        return self.__harga
+        
     @abstractmethod
     def show_info(self):
         pass
