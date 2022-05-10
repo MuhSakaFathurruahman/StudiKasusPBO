b1cb0a80f870aaeeda604b4b0cd116fbe328f2ae
damiri127 <damiricepri127@gmail.com>
2022-05-10 15:56:53 +0700

    ini stok

---
+++ stok.py
@@ -0,0 +1,14 @@
+from produk import Produk
+
+class Stok(Produk):
+    
+    def __init__(self, produk, qty):
+        self.__produk = produk
+        self.__qty = qty
+        
+    def get_produk(self):
+        return self.__produk
+    
+    def get_qty(self):
+        return self.__qty
+    
