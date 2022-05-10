eef6e66e8486c1a560810a796d2cb1e73ba4b87e
ginanazwa <nazwagina02@gmail.com>
2022-05-10 15:45:06 +0700

    perubahan gina 2

--- minyak.py
+++ minyak.py
@@ -1,12 +1,20 @@
 from abc import ABC, abstractmethod
+from produk import Produk
 
-class Minyak:
+class Minyak(Produk):
     
-    def  __init__ ( self , nama, harga ):
-         self. __nama  =   nama
-         self. __harga  =  harga
+     def set_nama(self, nama : str):
+       self.__nama = nama
         
+     def get_nama(self):
+        return self.__nama
     
-    @abstractmethod
-    def  show_info ( self ):
-        pass
+     def set_harga(self, harga :float):
+        self.__harga = harga
+        
+     def get_harga(self):
+        return self.__harga
+
+     def show_info(self,nama,harga):
+         print(self.__nama)
+         print(self.__harga)
